ca787d79c83c9c7392080da1a91e63f3f9d57fc1
CKiilu <cndeti@gmail.com>
2016-03-21 10:30:28 +0300

    Working with files saved in sdcard

--- app/src/main/java/com/scurrae/chris/quickpix/MainActivity.java
+++ app/src/main/java/com/scurrae/chris/quickpix/MainActivity.java
@@ -1,17 +1,25 @@
 package com.scurrae.chris.quickpix;
 
+import android.content.Intent;
 import android.graphics.Camera;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
-    Button button;
-    ImageView imageView;
+    private Button button;
+    private ImageView imageView;
+    private DatabaseHandler db;
+    static final int CAM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +30,42 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Intent to open camera app
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // Accessing the image file
+                File file = getFile();
+                // Returning the image from camera
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                // Begin camera activity
+                startActivityForResult(camera_intent, CAM_REQUEST);
 
             }
         });
+    }
+    private File getFile(){
+        // Instanciate file
+        File folder = new File("sdcard/camera_app");
+        boolean success = false;
+        if(!folder.exists()){
+            // Create folder if it doesn't exist
+            success = folder.mkdir();
+        }
+        if(!success){
+            Log.d("MAIN", "Folder not Created");
+        } else {
+            Log.d("MAIN", "Folder  Created");
+        }
+        // Create image file
+        File image_file = new File(folder, "cam_image.jpg");
+
+        return image_file;
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String path = "sdcard/camera_app/cam_image.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
+
     }
 }
