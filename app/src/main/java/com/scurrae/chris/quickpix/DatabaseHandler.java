022045ba7e3f0762eda9f860c8716f5823d5aeef
CKiilu <cndeti@gmail.com>
2016-03-21 09:38:55 +0300

    Data model created

--- app/src/main/java/com/scurrae/chris/quickpix/DatabaseHandler.java
+++ app/src/main/java/com/scurrae/chris/quickpix/DatabaseHandler.java
@@ -1,7 +1,36 @@
 package com.scurrae.chris.quickpix;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by chris on 3/21/16.
  */
-public class DatabaseHandler {
+public class DatabaseHandler extends SQLiteOpenHelper {
+    // Static vars
+    // DB version
+    private  static final int DATABASE_VERSION = 1;
+    // DB name
+    private  static final String DATABASE_NAME = "picturesManager";
+    // Contacts table name
+    private  static final String TABLE_CONTACTS = "pictures";
+    // Tables column names
+    private  static final String KEY_ID = "id";
+    private  static final String KEY_NAME = "picture";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
--- app/src/main/java/com/scurrae/chris/quickpix/Images.java
+++ app/src/main/java/com/scurrae/chris/quickpix/Images.java
@@ -4,4 +4,32 @@ package com.scurrae.chris.quickpix;
  * Created by chris on 3/21/16.
  */
 public class Images {
+    int _id;
+    int _image;
+
+    // Empty constructor
+    public Images(){
+
+    }
+    // Constructor with id and image as parameters
+    public Images(int id, int image){
+        this._id = id;
+        this._image = image;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public int get_image() {
+        return _image;
+    }
+
+    public void set_image(int _image) {
+        this._image = _image;
+    }
 }
